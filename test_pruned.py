47b0f1e5fb94b349ef93472b40363c44b57c2d85
YounHo  Jang <2014104142@khu.ac.kr>
2023-01-19 20:39:15 +0900

    yolo_structure4

--- test_pruned.py
+++ test_pruned.py
@@ -130,7 +130,7 @@ def test(data,
     # Configure  
     model.eval() 
     if isinstance(data, str):
-        is_coco = data.endswith('coco.yaml')
+        is_coco = data.endswith('coco.yaml') 
         with open(data) as f:
             data = yaml.load(f, Loader=yaml.SafeLoader)
     check_dataset(data)  # check
