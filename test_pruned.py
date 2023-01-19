f4e4d6b25b8e12c7846d557b0a9780177361577c
YounHo  Jang <2014104142@khu.ac.kr>
2023-01-19 20:29:52 +0900

    Structured_Pruning5

--- test_pruned.py
+++ test_pruned.py
@@ -61,7 +61,7 @@ def test(data,
     #     save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
     #     (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
   
-    #     # Load model
+    #     # Load model 
     #     model = attempt_load(weights, map_location=device)  # load FP32 model
     #     gs = max(int(model.stride.max()), 32)  # grid size (max stride)
     #     imgsz = check_img_size(imgsz, s=gs)  # check img_size
