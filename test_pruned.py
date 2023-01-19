b182356aafc05c3fb20e56fd48bb0368bcf36e58
YounHo  Jang <2014104142@khu.ac.kr>
2023-01-19 20:30:00 +0900

    Structured_Pruning6

--- test_pruned.py
+++ test_pruned.py
@@ -76,7 +76,7 @@ def test(data,
         # Directories
         save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
         (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
-        
+          
         # Load model
         #model = attempt_load(weights, map_location=device)  # load FP32 model
         ckpt = torch.load(weights[0], map_location=device)  # load checkpoint
